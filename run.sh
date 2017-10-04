60d43b397183b7470f68ff2ec17c6290969f9196
elkaian36 <32521411+elkaian36@users.noreply.github.com>
2017-10-04 18:46:48 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-./nheqminer/Linux_cmake/nheqminer_cpu/nheqminer_cpu -l eu1-zcash.flypool.org:3333 -u $ADDRESS.$HOSTNAME
+./nheqminer/Linux_cmake/nheqminer_cpu/nheqminer_cpu -l btcz.suprnova.cc:5586 -u elkaian1.1
